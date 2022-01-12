0d707c9761e846e57c578fce5df71330714f93d6
ramigou <dasul1021@gmail.com>
2022-01-12 15:54:58 +0900

    make blockchain using class

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,26 @@
-class Human {
-  public name: string
-  public age: number
-  public gender: string
-
-  constructor(name: string, age: number, gender?: string) {
-    this.name = name
-    this.age = age
-    this.gender = gender
+class Block {
+  public index: number
+  public hash: string
+  public previousHash: string
+  public data: string
+  public timestamp: number
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index
+    this.hash = hash
+    this.previousHash = previousHash
+    this.data = data
+    this.timestamp = timestamp
   }
 }
 
-const person = new Human('nana', 10)
+const genesisBlock: Block = new Block(0, 'fe34123fsef', '', 'hello!', 12345)
 
-const sayHi = (person: Human): string => {
-  return `hello ${person.name}, you are ${person.age}, ${person.gender}!!`
-}
+let blockchain: [Block] = [genesisBlock]
 
-const data = sayHi(person)
-console.log(data)
+console.log(blockchain)
