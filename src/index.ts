261965ef0c5f22f6d6d986b352459451456cb059
ramigou <dasul1021@gmail.com>
2022-01-12 15:43:37 +0900

    using class

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
-interface Human {
-  name: string
-  age: number
-  gender: string
+class Human {
+  public name: string
+  public age: number
+  public gender: string
+
+  constructor(name: string, age: number, gender?: string) {
+    this.name = name
+    this.age = age
+    this.gender = gender
+  }
 }
 
-const person = {
-  name: '철수',
-  age: 30,
-  gender: 'male',
-}
+const person = new Human('nana', 10)
 
 const sayHi = (person: Human): string => {
   return `hello ${person.name}, you are ${person.age}, ${person.gender}!!`
